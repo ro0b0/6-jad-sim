eabc213076b6789782b74a3d8c88b7e0db314fd2
robbertsteggerda <20216092@student.hhs.nl>
2021-03-23 21:43:00 +0100

    overhauled most the methods

--- src/Jad.java
+++ src/Jad.java
@@ -3,19 +3,25 @@ import java.util.Random;
 public class Jad {
     private int hp;
     private int defenceLevel;
-    private int defenceRoll;
-    private int attackRoll;
     private Player killedBy;
 
     public Jad(int hp, int defence){
         this.hp = hp;
         this.defenceLevel = defence;
     }
+
+    public static Jad defaultJad(){
+        return new Jad(350,480);
+    }
     //A simple method that reduces the jad's defence level by 30% if the player lands a  dragon warhammer on it.
     public void hammerSpec(){
         if(killedBy.doesAttackHit(killedBy.getMaxMeleeAttackRoll(),this.getMaxDefenceRoll(), true)) {
             this.defenceLevel *= 0.7;
         }
+    }
+
+    public void printJadInfo(){
+        System.out.printf("\nJad's max defence roll is %s.",this.getMaxDefenceRoll());
     }
 
     public int getDefenceRoll() {
@@ -26,7 +32,7 @@ public class Jad {
     //This max roll is calculated here. A number rolled from 0 - maxRoll is  then used in the accuracy check
     //to check if you hit on the jad or not.
     public int getMaxDefenceRoll(){
-        int effectiveDefenceLevel = (int) Math.round(defenceLevel) + 9;
+        int effectiveDefenceLevel = Math.round(defenceLevel) + 9;
         return effectiveDefenceLevel * 64;
     }
 }
--- src/Player.java
+++ src/Player.java
@@ -1,69 +1,63 @@
 import java.util.Random;
+import java.util.Scanner;
 
 public class Player {
     private int crushBonus;
-    private int maxHit;
+    private int maxHitDwh;
     private int rangedBonus;
     private int attackLevel;
     private int rangedLevel;
-    private int maxAttackRoll;
     private boolean hasRigour;
     private boolean hasTwistedBow;
 
-    public int getCrushBonus() {
-        return crushBonus;
-    }
-
-    public void setCrushBonus(int crushBonus) {
-        this.crushBonus = crushBonus;
-    }
-
-    public int getRangedBonus() {
-        return rangedBonus;
-    }
-
-    public void setRangedBonus(int rangedBonus) {
+    public Player(int crushBonus,int rangedBonus,int rangedLevel,int attackLevel,int maxHitDwh,boolean hasRigour,boolean hasTwistedBow){
+        this.crushBonus =  crushBonus;
         this.rangedBonus = rangedBonus;
-    }
-
-    public int getAttackLevel() {
-        return attackLevel;
-    }
-
-    public void setAttackLevel(int attackLevel) {
-        this.attackLevel = attackLevel;
-    }
-
-    public int getRangedLevel() {
-        return rangedLevel;
-    }
-
-    public void setRangedLevel(int rangedLevel) {
         this.rangedLevel = rangedLevel;
-    }
-
-    public boolean isHasRigour() {
-        return hasRigour;
-    }
-
-    public void setHasRigour(boolean hasRigour) {
+        this.attackLevel = attackLevel;
+        this.maxHitDwh = maxHitDwh;
         this.hasRigour = hasRigour;
+        this.hasTwistedBow = hasTwistedBow;
     }
 
-    public int getMaxHit(){
-        return maxHit;
+    public static Player defaultPlayerTbowRigour(){
+        return new Player(190,188,112,118,69,true,true);
     }
 
-    public void setMaxHit(int maxHit){
-        this.maxHit = maxHit;
+    public static Player createPlayer(){
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter your crush bonus: ");
+        int crushBonus = scanner.nextInt();
+        System.out.print("Enter your ranged bonus: ");
+        int rangedBonus = scanner.nextInt();
+        System.out.print("Enter your ranged level: ");
+        int baseRangedLevel = scanner.nextInt();
+        int pottedRangedLevel = (int) (baseRangedLevel * 1.1 +4);
+        System.out.print("Enter your attack level: ");
+        int baseAttackLevel = scanner.nextInt();
+        int pottedAttackLevel = (int) (baseAttackLevel * 1.15 + 5);
+        System.out.print("Enter your max hit with the dragon warhammer: ");
+        int maxHitDwh = scanner.nextInt();
+        scanner.nextLine();
+        System.out.print("Are you using rigour? (Y/N): ");
+        String answer = scanner.nextLine();
+        boolean hasRigour = answer.equals("Y");
+        System.out.print("Are you using a twisted bow? (Y/N): ");
+        answer = scanner.nextLine();
+        boolean hasTwistedBow = answer.equals("Y");
+
+        return new Player(crushBonus,rangedBonus,pottedRangedLevel,pottedAttackLevel,maxHitDwh,hasRigour,hasTwistedBow);
     }
 
-    public boolean isHasTwistedBow(){
-        return hasTwistedBow;
-    }
-
-    public void setHasTwistedBow(boolean hasTwistedBow){
-        this.hasTwistedBow =  hasTwistedBow;
+    public void printPlayerInfo(){
+        System.out.printf("\nYour crush bonus has been set at %s and your ranged bonus has been set at %s.",this.crushBonus,this.rangedBonus);
+        System.out.printf("\nYour ranged level while potted has been set to %s.",this.rangedLevel);
+        System.out.printf("\nYour attack level while potted has been set to %s.",this.attackLevel);
+        System.out.printf("\nYour max hit with the dragon warhammer has been set to %s.",this.maxHitDwh);
+        System.out.printf("\nUsing rigour: %s.",this.hasRigour);
+        System.out.printf("\nUsing a twisted bow: %s.",this.hasTwistedBow);
+        System.out.printf("\nYour max attack roll with the twisted bow is %s.",this.getMaxRangedAttackRoll());
+        System.out.printf("\nYour max attack roll with the dragon warhammer in inquisitor's is %s.",this.getMaxMeleeAttackRoll());
     }
 
     //The game calculates a max attack roll based on your attack bonuses, stats, prayers and set bonuses.
@@ -72,21 +66,21 @@ public class Player {
     public int getMaxRangedAttackRoll() {
         double rigour = 1;
         double twistedBow = 1;
-        if(this.isHasRigour()){
+        if(this.hasRigour){
             rigour = 1.2;
         }
 
         if(this.hasTwistedBow){
             twistedBow = 1.4;
         }
-        int effectiveRangedLevel = (int) Math.floor(this.getRangedLevel() * rigour) + 8;
-        double attackRoll = effectiveRangedLevel * (this.getRangedBonus() + 64) * twistedBow;
+        int effectiveRangedLevel = (int) Math.floor(this.rangedLevel * rigour) + 8;
+        double attackRoll = effectiveRangedLevel * (this.rangedBonus + 64) * twistedBow;
         return (int) attackRoll;
     }
 
     public int getMaxMeleeAttackRoll(){
-        int effectiveAttackLevel = (int) Math.floor(this.getAttackLevel() * 1.2) + 11;
-        double attackRoll = effectiveAttackLevel * (this.getCrushBonus() + 64) * 1.025;
+        int effectiveAttackLevel = (int) Math.floor(this.attackLevel * 1.2) + 11;
+        double attackRoll = effectiveAttackLevel * (this.crushBonus + 64) * 1.025;
         return (int) attackRoll;
     }
 
@@ -101,7 +95,7 @@ public class Player {
         if(maxAttackRoll>maxDefenceRoll) {
             accuracy = 1 - (((double) maxDefenceRoll + 2) / (2 * ((double) maxAttackRoll + 1)));
             if(dwh){
-                int dwhHit = random.nextInt(this.getMaxHit() + 1);
+                int dwhHit = random.nextInt(this.maxHitDwh + 1);
                 if(dwhHit == 0){
                     return false;
                 }
@@ -109,7 +103,7 @@ public class Player {
         }else{
             accuracy = (double) maxAttackRoll / (2 * ((double) maxDefenceRoll + 1));
             if(dwh){
-                int dwhHit = random.nextInt(this.getMaxHit() + 1);
+                int dwhHit = random.nextInt(this.maxHitDwh + 1);
                 if(dwhHit == 0){
                     return false;
                 }
